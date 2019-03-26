6843a60c723fcdbc035a9887bb9afb82f0e3054b
Baptiste Gombert <gombert@unistra.fr>
2019-03-26 09:46:22 +0000

    Transorm in proper module

---
+++ __init__.py
@@ -0,0 +1,13 @@
+'''
+Constructor file for PhaseCoherence
+B. Gombert - March 2019
+'''
+
+# Import PhaseCoherence 
+from . import PhaseCoherence
+
+# Import post processing routines
+from . import Cppostproc
+
+# Import plotting routines
+from . import PlotCp
